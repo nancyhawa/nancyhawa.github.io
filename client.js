9af4c7e6f64d6687dda1e54fd7b4967e63791a3d
Glitch (nancyhawa) <none>
2020-06-12 00:13:23 +0000

    👘🐚 Updated with Glitch

---
+++ client.js
@@ -0,0 +1,49 @@
+$(function() {
+  
+  var data = {
+    
+    objective: "To write clean, readable, reusable code with a team of engaged, creative, and thoughtful developers; to learn until my brain hurts, then to learn some more, and to help others do the same.",
+    familiarTech: [
+      "Ruby, Rails, JavaScript, NodeJS, jQuery, jQuery UI, React, Knockout, MySQL, MSSQL"
+    ],
+    experience: [
+      {
+        organization: "Fog Creek Software",
+        title: "Support Engineer, Support Team Lead, Software Developer",
+        dates: "January 2016 - Present",
+        bullets: [
+          "Updated FogBugz API to expand functionality (C#), giving users read/write access to user group and permissions data and holiday schedules rewrote API documentation, wrote a JavaScript wrapper to make working with the API more friendly, and wrote sample apps using API",
+          "Created marketing page to highlight product features",
+          "Updated agile development interface of FogBugz, using jQuery, jQuery UI, and KnockOut",
+          "Responded to customer tickets, troubleshooting a range of issues, including Windows Server downtime, mail server failures, and SQL Server failures",
+          
+        ]
+        
+      }
+    ],
+    projects: [
+      {
+        title: "Title",
+        description: "description",
+        projectURL: "url",
+        codeURL: "url"
+      }
+    ],
+    education: [
+      {
+        institution: "Amherst College",
+        degree: "B.A. Psychology, 2008"
+      },
+      {
+        institution: "Columbia Law School",
+        degree: "Attended 2005 - 2008"
+      },
+      {
+        institution: "FlatironSchool",
+        degree: "July 2015 - October 2015"
+      }
+    ]
+  }
+  
+  
+})
