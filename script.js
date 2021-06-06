b6577ab565b6c3a732b2429b1d421b7aa8db043c
Glitch (resume-retry) <none>
2021-06-06 03:58:11 +0000

    tweaks

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 $(function() {
     let square = '<div class="square"></div>'
     let screenWidth = $('body').width();
-    let squareCount = (screenWidth / 10 ) * 100;
+    let squareCount = (screenWidth / 10 ) * 70;
+    let activeSquareCount = squareCount/30
     for (let x=0; x < squareCount; x++) {
       $('#squares').append(square)
     }
   
     let squares = $('.square')
     let title = $('#title')
-    
+    $(title).fadeIn(2000)
+  
     let confettiEffect = function(){
-      let random = Math.floor(Math.random()*(squares.length));
+      let random = Math.floor(Math.random()*squareCount);
       let $element = $(squares[random]) 
-      $(title).fadeIn(2000)
+      
       $element
         .removeClass(classes.join(" "))
         .addClass(classes[Math.floor(Math.random()*classes.length)])
@@ -26,7 +28,7 @@ $(function() {
     let clickConfettiEffect = function(){
       $(title).fadeIn(2000)
       
-      for (let i=0; i < 300; i++){
+      for (let i=0; i < activeSquareCount; i++){
         let random = Math.floor(Math.random()*(squares.length));
         let $element = $(squares[random]) 
         let randomTime = Math.floor(Math.random()*3000);
